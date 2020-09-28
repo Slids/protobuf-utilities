6a7352f071cfd3d0e810e86f7ca3afa48b0bbe03
jgodbou <jgodbou@penguin>
2020-09-28 00:27:55 -0400

    cgay review updates

--- protobuf-utilities.lisp
+++ protobuf-utilities.lisp
@@ -7,31 +7,31 @@
 (in-package :protobuf-utilities)
 
 (defun deserialize-proto-from-base64-string (proto-type proto-string)
-  "Given a string containing a proto PROTO-STRING and
-it's desired type PROTO-TYPE deserialize the proto."
+  "Deserialize a proto of type PROTO-TYPE from the base-64
+encoded string PROTO-STRING."
   (declare (type string proto-string))
   (let ((bytes (flexi-streams:string-to-octets
                 (cl-base64:base64-string-to-string proto-string))))
-    (cl-protobufs:deserialize-object-from-bytes
+    (cl-protobufs:deserialize-from-bytes
      proto-type bytes)))
 
 (defun serialize-proto-to-base64-string (proto)
   "Serialize a PROTO to octets and call base64 encode"
   (cl-base64:string-to-base64-string
    (flexi-streams:octets-to-string
-    (cl-protobufs:serialize-object-to-bytes proto))))
+    (cl-protobufs:serialize-to-bytes proto))))
 
-(defmacro with-deserialized-protos (arg-arg-type-list &body body)
-  "Take a list (ARGUMENT . PROTO-TYPE) arg-arg-type-list that
+(defmacro with-deserialized-protos (message-message-type-list &body body)
+  "Take a list (MESSAGE . PROTO-TYPE) MESSAGE-MESSAGE-TYPE-LIST that
 will exist in the enclosing scope and create a scope where
 the bound paramaters will be deserialized. Expects
 body to return a proto that it will then serialize and call
 base64 encode on."
   (let ((result-proto (gensym "RESULT-PROTO")))
-    `(let ,(loop for (arg . arg-type) in  arg-arg-type-list
+    `(let ,(loop for (message . message-type) in  message-message-type-list
                  collect
-                 `(,arg (deserialize-proto-from-base64-string
-                         ,arg-type
-                         (or ,arg ""))))
+                 `(,message (deserialize-proto-from-base64-string
+                             ,message-type
+                             (or ,message ""))))
        (let ((,result-proto ,@body))
          (serialize-proto-to-base64-string ,result-proto)))))
