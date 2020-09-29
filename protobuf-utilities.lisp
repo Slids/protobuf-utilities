a8c4a9a03c65a19b56495bd2761e79c4ffd1ebe5
jgodbou <jgodbou@penguin>
2020-09-28 21:56:14 -0400

    Updates to with-deserialized-protos-serializing-return

--- protobuf-utilities.lisp
+++ protobuf-utilities.lisp
@@ -2,7 +2,9 @@
   (:use #:cl)
   (:export #:serialize-proto-to-base64-string
            #:deserialize-proto-from-base64-string
-           #:with-deserialized-protos))
+           #:with-deserialized-protos
+           #:serialize-result
+           #:with-deserialized-protos-serializing-return))
 
 (in-package :protobuf-utilities)
 
@@ -22,16 +24,30 @@ encoded string PROTO-STRING."
     (cl-protobufs:serialize-to-bytes proto))))
 
 (defmacro with-deserialized-protos (message-message-type-list &body body)
-  "Take a list (MESSAGE . PROTO-TYPE) MESSAGE-MESSAGE-TYPE-LIST that
-will exist in the enclosing scope and create a scope where
-the bound paramaters will be deserialized. Expects
-body to return a proto that it will then serialize and call
-base64 encode on."
+  "Take a list (MESSAGE . PROTO-TYPE) MESSAGE-MESSAGE-TYPE-LIST
+where the message will be a symbol pointing to a base64-encoded
+serialized proto in a string. Deserialize the protos and store
+them in the message symbols.
+
+The messages are bound lexically so after this macro finishes the
+protos return to be serialized base64-encoded strings."
+  `(let ,(loop for (message . message-type) in  message-message-type-list
+               collect
+               `(,message (deserialize-proto-from-base64-string
+                           ,message-type
+                           (or ,message ""))))
+     ,@body))
+
+(defmacro serialize-result (&body body)
+  "Serialize the result of calling BODY into a base64-encoded string."
   (let ((result-proto (gensym "RESULT-PROTO")))
-    `(let ,(loop for (message . message-type) in  message-message-type-list
-                 collect
-                 `(,message (deserialize-proto-from-base64-string
-                             ,message-type
-                             (or ,message ""))))
-       (let ((,result-proto ,@body))
-         (serialize-proto-to-base64-string ,result-proto)))))
+    `(let ((,result-proto ,@body))
+       (serialize-proto-to-base64-string ,result-proto))))
+
+(defmacro with-deserialized-protos-serializing-return (message-message-type-list &body body)
+  "Wrapper combining WITH-DESERIALIZED-PROTOS and serialize result of BODY.
+
+Useful in an http server context where you know your inputs will be protos which
+are contained in base64-encoded strings and you will want to return a base64 encoded
+proto."
+  `(serialize-result (with-deserialized-protos ,message-message-type-list ,@body)))
